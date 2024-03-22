66de7fd9c6e92e368b5f22c34f392eb3246fba37
Toru Otani <ohtanitoru@gmail.com>
2024-03-22 09:07:01 +0900

    add: コードのシンタックスハイライト機能追加

--- components/post-title.js
+++ components/post-title.js
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }) {
   return (
-    <h1 className="my-16 text-4xl font-bold leading-tight tracking-tighter md:text-5xl lg:text-6xl">
+    <h1 className="my-16 text-4xl font-bold leading-tight md:text-5xl lg:text-6xl">
       {children}
     </h1>
   )
