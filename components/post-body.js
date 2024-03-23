a8436975300a4be0f882fddfd8c0ddfbe3143141
Toru Otani <ohtanitoru@gmail.com>
2024-03-24 02:09:49 +0900

    fix: コードのシンタックスハイライト機能修正

--- components/post-body.js
+++ components/post-body.js
@@ -1,14 +1,15 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
-import { BLOCKS, INLINES, MARKS } from '@contentful/rich-text-types'
+import { BLOCKS, INLINES } from '@contentful/rich-text-types'
 import markdownStyles from './markdown-styles.module.css'
 import RichTextAsset from './rich-text-asset'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import Link from 'next/link'
 
 const customMarkdownOptions = (content, text) => ({
   renderNode: {
     [BLOCKS.EMBEDDED_ENTRY]: (node) => {
-      const entry = node.data.target.sys.contentType.sys.id;
+      const entry = node.data.target.sys.contentType.sys.id
 
       if (entry === "codeBlock") {
         const fileName = node.data.target.fields.fileName
@@ -43,11 +44,7 @@ const customMarkdownOptions = (content, text) => ({
     ),
     [INLINES.HYPERLINK]: (node, children) => {
       let anchorAttrs = {}
-      if (
-        !node.data.uri.includes(
-          'https://next-contentful-blog-tani-yan.vercel.app'
-        )
-      ) {
+      {
         anchorAttrs = {
           target: '_blank',
           rel: 'noopener noreferrer',
@@ -59,56 +56,14 @@ const customMarkdownOptions = (content, text) => ({
         </a>
       )
     },
-    // コードブロックをdivで括る
-    // [BLOCKS.PARAGRAPH]: (node, children) => {
-    //   if (node.content.length === 1 && node.content[0].marks.find(x => x.type === 'code')) {
-    //     return <div>{children}</div>
-    //   }
-    //   return <p>{children}</p>
-    // },
+    [INLINES.ENTRY_HYPERLINK]: (node, children) => {
+      return (
+        <Link href={`/entry/${node.data.target.fields.slug}`}>
+          <a className="hover:underline">{children}</a>
+        </Link>
+      )
+    },
   },
-  // renderText: (text) => {
-  //   return text.split('\n').reduce((children, textSegment, index) => {
-  //     return [...children, index > 0 && <br key={index} />, textSegment]
-  //   }, [])
-  // },
-  // renderMark: {
-  //   [MARKS.CODE]: (text) => {
-  //     text.shift() // コードブロックのfalseを削除
-  //     let language = text.shift() // コードブロックの1行目の言語指定をClassに利用後削除
-  //     const classList = language.indexOf(':') ? language.split(':') : []
-  //     language = classList[0]?.replace('language-', '')
-  //     const fileName = classList[1]
-  //     text.shift() // コードブロックの1行目の改行を削除
-
-  //     const value = text.reduce((acc, cur) => {
-  //       if (typeof cur !== "string" && cur.type === "br") {
-  //         return acc + "\n"
-  //       }
-  //       return acc + cur
-  //     }, "")
-
-  //     return (
-  //       <>
-  //         {fileName && (
-  //           <div>
-  //             <span>{fileName}</span>
-  //           </div>
-  //         )}
-  //         <SyntaxHighlighter 
-  //           language={language} 
-  //           style={dracula}
-  //           customStyle={{
-  //             fontSize: '0.8125em',
-  //             lineHeight: '1.692'
-  //           }}
-  //         >
-  //           {value}
-  //         </SyntaxHighlighter>
-  //       </>
-  //     )
-  //   },
-  // },
 })
 
 export default function PostBody({ content, about, author }) {
